a47737a5140986a426be8902bf6d4ed69581610a
Asbjorn Riedel <Asbjorn.Riedel@Abraxas.ch>
2020-11-24 20:11:24 +0100

    Using APR1-MD5 algorithm for apache > 2.2.18 Corrected commenting in config.ini

--- sites/html/htadmin/tools/htpasswd.php
+++ sites/html/htadmin/tools/htpasswd.php
@@ -22,7 +22,7 @@ class htpasswd {
 		$htaccessfile = $path . "/" . self::HTACCESS_NAME;
 		$htpasswdfile = $path . "/" . self::HTPASSWD_NAME;
 		@$this->use_metadata = $use_metadata;
-		
+
 		if (! file_exists ( $htaccessfile )) {
 			$bdfp = fopen ( $htaccessfile, 'w' );
 			$htaccess_content = "AuthType Basic\nAuthName \"Password Protected Area\"\nAuthUserFile \"" . $htpasswdfile . "\"\nRequire valid-user" . "\n<Files .ht*>\nOrder deny,allow\nDeny from all\n</Files>";
@@ -30,7 +30,7 @@ class htpasswd {
 		}
 		
 		@$this->fp = @$this::open_or_create ( $htpasswdfile );
-		
+
 		if ($use_metadata) {
 			$htmetafile = $path . "/" . self::HTMETA_NAME;
 			@$this->metafp = @$this::open_or_create ( $htmetafile );
@@ -184,11 +184,11 @@ class htpasswd {
 		$fp = fopen ( $filename, 'r+' );
 		return true;
 	}
+
 	static function htcrypt($password) {
-		return crypt ( $password, substr ( str_replace ( '+', '.', base64_encode ( pack ( 'N4', mt_rand (), mt_rand (), mt_rand (), mt_rand () ) ) ), 0, 22 ) );
+		return self::crypt_apr1_md5($password);
 	}
 
-	
 	static function create_hash_tool($hash) {
 		if (strpos($hash, '$apr1') === 0) {
 			return new md5_hash_tool();
@@ -196,8 +196,36 @@ class htpasswd {
 			return new crypt_hash_tool();
 		}
 	}
-		
-}	
-	
+	// APR1-MD5 encryption method (windows compatible)
+	static function crypt_apr1_md5($plainpasswd) {
+		$salt = substr(str_shuffle("abcdefghijklmnopqrstuvwxyz0123456789"), 0, 8);
+		$len = strlen($plainpasswd);
+		$text = $plainpasswd.'$apr1$'.$salt;
+		$bin = pack("H32", md5($plainpasswd.$salt.$plainpasswd));
+		for($i = $len; $i > 0; $i -= 16) { $text .= substr($bin, 0, min(16, $i)); }
+		for($i = $len; $i > 0; $i >>= 1) { $text .= ($i & 1) ? chr(0) : $plainpasswd{0}; }
+		$bin = pack("H32", md5($text));
+		for($i = 0; $i < 1000; $i++)
+		{
+			$new = ($i & 1) ? $plainpasswd : $bin;
+			if ($i % 3) $new .= $salt;
+			if ($i % 7) $new .= $plainpasswd;
+			$new .= ($i & 1) ? $bin : $plainpasswd;
+			$bin = pack("H32", md5($new));
+		}
+		for ($i = 0; $i < 5; $i++)
+		{
+			$k = $i + 6;
+			$j = $i + 12;
+			if ($j == 16) $j = 5;
+			$tmp = $bin[$i].$bin[$k].$bin[$j].$tmp;
+		}
+		$tmp = chr(0).chr(0).$bin[11].$tmp;
+		$tmp = strtr(strrev(substr(base64_encode($tmp), 2)),
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+				"./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
 
+		return "$"."apr1"."$".$salt."$".$tmp;
+	}
+}	
 ?>
